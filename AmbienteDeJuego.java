b4c7c733881f0304c07c5d1897ecadeff06f265f
eregar <30888167+eregar@users.noreply.github.com>
2018-04-25 11:41:39 -0500

    Add files via upload

--- AmbienteDeJuego.java
+++ AmbienteDeJuego.java
@@ -7,7 +7,7 @@ import java.awt.event.KeyListener;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
-public class AmbienteDeJuego extends JPanel implements KeyListener{
+public class AmbienteDeJuego extends JPanel implements KeyListener, Runnable{
 	private NaveJugador nJ;
 	private boolean w,a,s,d;
 	public AmbienteDeJuego () {
@@ -15,12 +15,14 @@ public class AmbienteDeJuego extends JPanel implements KeyListener{
 		this.setPreferredSize(new Dimension(800, 650));
 		
 		this.nJ = new NaveJugador();
+		Thread hilo=new Thread(this);
 		this.w=false;
 		this.a=false;
 		this.s=false;
 		this.d=false;
 		
 		this.addKeyListener(this);
+		hilo.start();
 	}
 	
 	
@@ -40,27 +42,6 @@ public class AmbienteDeJuego extends JPanel implements KeyListener{
 		}else if(e.getKeyCode() == KeyEvent.VK_DOWN && !this.s) {
 			this.s=true;
 		}
-		if(this.w){
-			if(this.nJ.getNY() + 500 >= 5) {
-				this.nJ.setNY(-7);
-			}
-		}
-		if(this.a){
-			if(this.nJ.getNX() + 300 >= 5) {
-				this.nJ.setNX(-7);
-			}
-		}
-		if(this.s){
-			if(this.nJ.getNY() + 500 <= 600) {
-				this.nJ.setNY(7);
-			}
-		}
-		if(this.d){
-			if(this.nJ.getNX() + 300 <= 755) {  
-				this.nJ.setNX(7);
-			}
-		}
-		repaint();
 	}
 
 
@@ -82,5 +63,36 @@ public class AmbienteDeJuego extends JPanel implements KeyListener{
 	@Override
 	public void keyTyped(KeyEvent e) {
 	}
-	
+
+
+	@Override
+	public void run() {
+		try {
+			while (true){
+			if(this.w){
+				if(this.nJ.getNY() + 500 >= 5) {
+					this.nJ.setNY(-4);
+				}
+			}
+			if(this.a){
+				if(this.nJ.getNX() + 300 >= 5) {
+					this.nJ.setNX(-7);
+				}
+			}
+			if(this.s){
+				if(this.nJ.getNY() + 500 <= 600) {
+					this.nJ.setNY(4);
+				}
+			}
+			if(this.d){
+				if(this.nJ.getNX() + 300 <= 755) {  
+					this.nJ.setNX(7);
+				}
+			}
+			Thread.sleep(10);
+			repaint();
+			}} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
