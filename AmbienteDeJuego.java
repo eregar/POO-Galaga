1932f6d3313d79704f9cb58a05413009fc3beb30
eregar <30888167+eregar@users.noreply.github.com>
2018-04-24 20:05:50 -0500

    Add files via upload

--- AmbienteDeJuego.java
+++ AmbienteDeJuego.java
@@ -9,12 +9,16 @@ import javax.swing.JPanel;
 
 public class AmbienteDeJuego extends JPanel implements KeyListener{
 	private NaveJugador nJ;
-	
+	private boolean w,a,s,d;
 	public AmbienteDeJuego () {
 		super();
 		this.setPreferredSize(new Dimension(800, 650));
 		
 		this.nJ = new NaveJugador();
+		this.w=false;
+		this.a=false;
+		this.s=false;
+		this.d=false;
 		
 		this.addKeyListener(this);
 	}
@@ -27,34 +31,51 @@ public class AmbienteDeJuego extends JPanel implements KeyListener{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			if(this.nJ.getNX() + 300 <= 755) {  
-				this.nJ.setNX(7);
-				repaint();
-			}
-		}else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-			if(this.nJ.getNX() + 300 >= 5) {
-				this.nJ.setNX(-7);
-				repaint();
-			}
-		}else if(e.getKeyCode() == KeyEvent.VK_UP) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT && !this.d) {
+			this.d=true;
+		}else if(e.getKeyCode() == KeyEvent.VK_LEFT && !this.a) {
+			this.a=true;
+		}else if(e.getKeyCode() == KeyEvent.VK_UP && !this.w) {
+			this.w=true;
+		}else if(e.getKeyCode() == KeyEvent.VK_DOWN && !this.s) {
+			this.s=true;
+		}
+		if(this.w){
 			if(this.nJ.getNY() + 500 >= 5) {
 				this.nJ.setNY(-7);
-				repaint();
-			}
-		}else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
-			if(this.nJ.getNY() + 500 <= 600) {
-				this.nJ.setNY(7);
-				repaint();
 			}
 		}
+		if(this.a){
+			if(this.nJ.getNX() + 300 >= 5) {
+				this.nJ.setNX(-7);
+			}
+		}
+		if(this.s){
+			if(this.nJ.getNY() + 500 <= 600) {
+				this.nJ.setNY(7);
+			}
+		}
+		if(this.d){
+			if(this.nJ.getNX() + 300 <= 755) {  
+				this.nJ.setNX(7);
+			}
+		}
+		repaint();
 	}
 
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			this.d=false;
+		}else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			this.a=false;
+		}else if(e.getKeyCode() == KeyEvent.VK_UP) {
+			this.w=false;
+		}else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+			this.s=false;
+		}
 	}
 
 
